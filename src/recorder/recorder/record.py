37409b3a2ee69f3973fdebddbb82b06496849472
NorwegianIcecube <patkar07.pk@gmail.com>
2022-11-14 15:23:15 +0100

    recorder package

--- src/recorder/recorder/record.py
+++ src/recorder/recorder/record.py
@@ -13,8 +13,6 @@ def main():
 
             # write the flipped frame
             out.write(frame)
-
-            cv2.imshow('frame',frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
