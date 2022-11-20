4d274b0dc4418b47d44b2ddd031a146d9d2a0634
NorwegianIcecube <patkar07.pk@gmail.com>
2022-11-20 17:05:39 +0100

    birds eye view

--- src/turtlebot/turtlebot/controller_publisher_utils.py
+++ src/turtlebot/turtlebot/controller_publisher_utils.py
@@ -138,8 +138,8 @@ def pipeline(img, points, turn):
     
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     h, w = img_gray.shape
-    img_blur = cv2.blur(img_gray, (1,1))
-    img_canny = cv2.Canny(img_blur, 0, 200)
+    img_blur = cv2.blur(img_gray, (3,3))
+    img_canny = cv2.Canny(img_blur, 0, 200, L2gradient=True)
    
     #valTrackbars(IMAGE_WIDTH, IMAGE_HEIGHT)
     img_warp = warp_img(img_canny, points, h, w)
